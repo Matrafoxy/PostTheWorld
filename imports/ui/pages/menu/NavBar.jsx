5874dd8073d21d9d729a5195c8990293b23b2e15
tudor_b <tburduhos@yahoo.com>
2018-03-29 19:32:40 +0300

    add register button

--- imports/ui/pages/menu/NavBar.jsx
+++ imports/ui/pages/menu/NavBar.jsx
@@ -12,63 +12,71 @@ import {NavMenuRoutes} from '/imports/api/menu/routes';
 
 class NavBar extends React.Component{
 
-	constructor(props){
-		super(props);
-		this.state = {
-			activeItem: 'home'
-		}
-	}
+    constructor(props){
+        super(props);
+        this.state = {
+            activeItem: 'home'
+        }
+    }
 	
-	handleItemClick(name){
+    handleItemClick(name){
 			
-			this.setState({ activeItem: name });
+        this.setState({ activeItem: name });
 			
-			route.go(name);
-	}
+        route.go(name);
+    }
 
 
-	render(){
+    render(){
 
-		const { activeItem } = this.state;
+        const { activeItem } = this.state;
 
-		return(
-			<Menu inverted>
+        return(
+            <Menu inverted>
 			 <Menu.Item
-			 	 name={ NavMenuRoutes.HOME } 
-			 	 active={ activeItem === NavMenuRoutes.HOME } 
-			 	 onClick={() => this.handleItemClick(NavMenuRoutes.HOME)} 
+			 	 name={ NavMenuRoutes.HOME }
+			 	 active={ activeItem === NavMenuRoutes.HOME }
+			 	 onClick={() => this.handleItemClick(NavMenuRoutes.HOME)}
 			 	 />
 	       
-	        <Menu.Item 
-	        	name={ NavMenuRoutes.POSTLIST } 
-	        	active={ activeItem === NavMenuRoutes.POSTLIST } 
-	        	onClick={() => this.handleItemClick(NavMenuRoutes.POSTLIST)} 
+	        <Menu.Item
+	        	name={ NavMenuRoutes.POSTLIST }
+	        	active={ activeItem === NavMenuRoutes.POSTLIST }
+	        	onClick={() => this.handleItemClick(NavMenuRoutes.POSTLIST)}
 	        	/>
 
 	        { !Meteor.userId() ?
-	           <Menu.Item
-	           		name={ NavMenuRoutes.LOGIN } 
-	           		active={ activeItem === NavMenuRoutes.LOGIN } 
-	           		onClick={() => this.handleItemClick(NavMenuRoutes.LOGIN)} />
+	        	<Menu.Menu>
+	        	<Menu.Item
+	           		name={ NavMenuRoutes.LOGIN }
+	           		active={ activeItem === NavMenuRoutes.LOGIN }
+	           		onClick={() => this.handleItemClick(NavMenuRoutes.LOGIN)} 
+	           		/>
+	           	<Menu.Item
+	           		name={ NavMenuRoutes.REGISTER }
+	           		active={ activeItem === NavMenuRoutes.REGISTER }
+	           		onClick={() => this.handleItemClick(NavMenuRoutes.REGISTER)} 
+	           		/>
+	        </Menu.Menu>
 	        :
 
 	        <Menu.Menu position='right'>
-	          <Menu.Item 
-	          	name={ NavMenuRoutes.POSTCREATE }  
-	          	active={ activeItem ===  NavMenuRoutes.POSTCREATE } 
-	          	onClick={() => this.handleItemClick(NavMenuRoutes.POSTCREATE)} 
+	          <Menu.Item
+	          	name={ NavMenuRoutes.POSTCREATE }
+	          	active={ activeItem ===  NavMenuRoutes.POSTCREATE }
+	          	onClick={() => this.handleItemClick(NavMenuRoutes.POSTCREATE)}
 	          	/>
 	          
-	          <Menu.Item 
-	          	name={ NavMenuRoutes.LOGOUT }  
-	          	active={ activeItem ===  NavMenuRoutes.LOGIN } 
-	          	onClick={() => this.handleItemClick(NavMenuRoutes.LOGOUT)} 
+	          <Menu.Item
+	          	name={ NavMenuRoutes.LOGOUT }
+	          	active={ activeItem ===  NavMenuRoutes.LOGIN }
+	          	onClick={() => this.handleItemClick(NavMenuRoutes.LOGOUT)}
 	          	/>
 	        </Menu.Menu>
 	        }
 	      </Menu>
 	      );
-	}
+    }
 }
 
 export default NavBar;
