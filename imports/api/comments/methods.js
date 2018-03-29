6d26c8631b144d21767bf85f8b9b304585460fa2
tudor_b <tburduhos@yahoo.com>
2018-03-29 20:19:45 +0300

    add comment service

--- imports/api/comments/methods.js
+++ imports/api/comments/methods.js
@@ -1,27 +1,20 @@
 import { Meteor } from 'meteor/meteor';
 
-import Comments from './collection';
+import CommentService from './services';
 
 
 Meteor.methods({
     'comment.list'(postId){
-        let comments = Comments.find({postId}).fetch();
-
+        let comments = CommentService._getComments(postId);
 
         return comments;
     },
 	
     'comment.add'(data){
-        if(!Meteor.userId())
-            throw new Meteor.Error(500, 'Error 500: Server error', "Client is not authenticated");
-        Comments.insert(data);
+       CommentService.createComment(data);
     },
 
     'comment.remove'(_id){
-        let comment  = Comments.findOne(_id);
-        if(Meteor.userId() !== comment.userId)
-            throw new Meteor.Error(500, 'Delete error', "You're not authorized to delete this comment");
-
-        Comments.remove(_id);
+        CommentService.deleteComment(_id);
     }
 });
---
+++ imports/api/comments/services.js
@@ -0,0 +1,27 @@
+import { Meteor } from 'meteor/meteor';
+
+import Comments from './collection';
+
+
+class CommentService {
+
+	static _getComments(postId){
+		return Comments.find({postId}).fetch();
+	}
+
+	static createComment(data){
+		if(!Meteor.userId())
+            throw new Meteor.Error(500, 'Error 500: Server error', "Client is not authenticated");
+        Comments.insert(data);
+	}
+
+	static deleteComment(_id){
+		let comment  = Comments.findOne(_id);
+        if(Meteor.userId() !== comment.userId)
+            throw new Meteor.Error(500, 'Delete error', "You're not authorized to delete this comment");
+
+        Comments.remove(_id);
+	}
+}
+
+export default CommentService;
